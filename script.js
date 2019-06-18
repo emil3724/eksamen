b5ee6a569388a3c55ca4e374a4f44fa4b71758fc
Emilie Ejersted Larsen <emilieejersted@Emilies-MacBook-Pro-10.local>
2019-06-19 00:59:18 +0200

    Ny burgermenu

--- script.js
+++ script.js
@@ -1,46 +1,43 @@
-window.addEventListener("load", sidenVises)
-
-//Når siden er loaded kald functionen sidenVises
+window.addEventListener("load", sidenVises);
 
 function sidenVises() {
-    //Udskriv at du er i denne funtion i consolen
     console.log("sidenVises");
-    //Kald startGame functionen
-    startGame();
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 }
 
-function startGame() {
-    //Udskriv at du er i denne funtion i consolen
-    console.log("startGame");
-    //Gør så man kan klikke på #man vælger
-    document.querySelector("#js_sprite").addEventListener("click", clickRotation);
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        document.querySelector("#menuknap").textContent = "X";
+    }
 }
 
-function clickRotation() {
-    //Udskriv at du er i denne funtion i consolen
-    console.log("clickRotation");
-    //tilføj classen forsvind til this
-    this.classList.add("rotation");
-    this.removeEventListener("click", clickRotation);
-}
-
-//Initialize the default bulb
 document.getElementById('change_emilie').src = 'img/emilie_1.jpg';
 
-//Grab on the switch on btn and add an event(click)
+//henter element fra id og gør at man kan klikke på den//
 document.getElementById('on').addEventListener('click', switchOn);
 
-//Grab on the switch off btn and add an event(click)
+//henter element fra id og gør at man kan klikke på den//
 document.getElementById('off').addEventListener('click', switchOff);
 
-//Runs when the switch on button is clicked
+//funktion der kører når man er på on-knap
 function switchOn() {
+    //Udskriv at du er i denne funtion i consolen
+    console.log("switchOn")
     let on = document.getElementById('change_emilie');
     on.src = 'img/emilie_1.jpg';
 }
 
-//Runs when the switch off button is clicked
+//funktion der kører når man er på off-knap
 function switchOff() {
+    //Udskriv at du er i denne funtion i consolen
+    console.log("switchOff");
     let Off = document.getElementById('change_emilie');
     Off.src = 'img/emilie_2.jpg';
 }
